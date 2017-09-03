b157627eb8ff2c0bb81be6c19ca9dcce071f9a1e
lois-lee <ll556@cornell.edu>
2017-09-02 21:33:57 -0400

    Update codeforsrvo.ino

--- docs/lab1/codeforsrvo.ino
+++ docs/lab1/codeforsrvo.ino
@@ -1,1 +1,28 @@
-to be finished
+#include <Servo.h>
+Servo myservo;
+int LED= 9;
+int PINNAME = A1;
+int VAL=0;
+int Y=0;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(LED);
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+ 
+  VAL=analogRead(PINNAME);
+  analogWrite(LED,VAL);
+  Y = map(VAL, 0, 1023, 0, 180);//map 0-1023 to 0-180 range
+  myservo.write(Y);
+  delay(500);
+  Serial.print("pot reading  ");
+  Serial.print(VAL);
+  Serial.print(" : ");
+  Serial.print(" speed and direction ");
+  Serial.println(Y);
+  
+}
