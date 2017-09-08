4b7ad0bdb64174f7da7479c1b0e64cd084bdccb6
Firehiwot <31547603+Firehiwot@users.noreply.github.com>
2017-09-08 11:04:41 -0400

    Update codeforsrvo.ino

--- docs/lab1/codeforsrvo.ino
+++ docs/lab1/codeforsrvo.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 Servo myservo;
-int LED= 9;
-int PINNAME = A1;
+int LED= 9; 
+int PINNAME = A1; 
 int VAL=0;
 int Y=0;
 // the setup function runs once when you press reset or power the board
@@ -12,7 +12,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
  
-  VAL=analogRead(PINNAME);
+  VAL=analogRead(PINNAME); // potentiometer reading
   analogWrite(LED,VAL);
   Y = map(VAL, 0, 1023, 0, 180);//map 0-1023 to 0-180 range
   myservo.write(Y);
