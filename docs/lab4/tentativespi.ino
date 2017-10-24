4658f05824bda92814c1789a85b9691b72e44a7d
lois-lee <ll556@cornell.edu>
2017-10-24 19:26:57 -0400

    Update tentativespi.ino

--- docs/lab4/tentativespi.ino
+++ docs/lab4/tentativespi.ino
@@ -3,7 +3,7 @@
   #include <SPI.h>
 
   // declare the slave pin 
-  uint8_t const ssFPGA = 10;
+  uint8_t ssFPGA = 10;
 
 void setup()
 {
@@ -12,16 +12,19 @@ void setup()
   pinMode (ssFPGA, OUTPUT);
   
   // settings for SPI:
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV32);
+//  SPI.setBitOrder(MSBFIRST);
+//  SPI.setDataMode(SPI_MODE0);
+//  SPI.setClockDivider(SPI_CLOCK_DIV32);
+
+  
   
   // initialize SPI:
   SPI.begin();
   
   // Serial setup:
   Serial.begin(9600);
-  
+  //set the pin high
+  digitalWrite(ssFPGA,HIGH);
   // print when it starts so we know
   Serial.println("Sending information...");  
   
@@ -34,67 +37,14 @@ void setup()
 
 void loop() 
 {
-    digitalWrite( ssFPGA, 0 );
-    for ( uint8_t i = 0; i < 10; i++) {
-        delay( 10 );
-        SPI.beginTransaction( SPISettings( 4000000, MSBFIRST, SPI_MODE3 ) );
-        {
-            
-  // include the SPI library:
-  #include <SPI.h>
+//byte data= to the information from got_data transferred
+//(for now we will use a dummy)
 
-  // declare the slave pin 
-  uint8_t const ssFPGA = 10;
+digitalWrite (ssFPGA,LOW);
+byte data;
+SPI.transfer(data);
+digitalWrite(ssFPGA,HIGH);
 
-void setup()
-{
-   
-  // set the slaveSelectPin as an output:
-  pinMode (ssFPGA, OUTPUT);
-  
-  // settings for SPI:
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV32);
-  
-  // initialize SPI:
-  SPI.begin();
-  
-  // Serial setup:
-  Serial.begin(9600);
-  
-  // print when it starts so we know
-  Serial.println("Sending information...");  
-  
-
-
- // Serial.println("Info sent!");  
-}
-
-}
-
-void loop() 
-{
-  
-  //This bottom part is probably wrong
-    digitalWrite( ssFPGA, 0 );
-    for ( uint8_t i = 0; i < 10; i++) {
-        delay( 10 );
-        SPI.beginTransaction( SPISettings( 4000000, MSBFIRST, SPI_MODE3 ) );
-        {
-            uint8_t const MISO = SPI.transfer(i == 0 ? 0xAA : 0x55 );
-            Serial.println( MISO, BIN );
-        }
-        SPI.endTransaction();
-    }
-    digitalWrite( ssFPGA, 1 );
-  }
-
-}
-        }
-        SPI.endTransaction();
-    }
-    digitalWrite( ssFPGA, 1 );
   }
 
 }
