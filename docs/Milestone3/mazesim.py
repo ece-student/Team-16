b06bc0686875db19759f240a0bb8a1542e8ea71c
Rohit Bandaru <RohitBandaru@users.noreply.github.com>
2017-11-10 16:01:39 -0500

    Update mazesim.py

--- docs/Milestone3/mazesim.py
+++ docs/Milestone3/mazesim.py
@@ -1,10 +1,14 @@
 import math
 import queue
+from graphics import *
+from time import sleep
+import random
 
 class MazeNode:
-	def __init__(self, x, y):
+	def __init__(self, x, y, maze):
 		self.x = x
 		self.y = y
+
 		self.up = True
 		self.down = True
 		self.left = True
@@ -13,8 +17,19 @@ class MazeNode:
 		self.visited = False
 		self.isCurr = False
 
+		self.maze = maze
+
 	def visit(self):
+		self.maze.currNode.isCurr=False
+		self.maze.currNode = self
 		self.visited = True
+		self.isCurr = True
+
+		self.maze.visitedNodes += 1
+		print(self.maze.visitedNodes)
+		if(self.maze.visitedNodes == self.maze.cols*self.maze.rows):
+			print("DONE!!!!")
+			self.maze.notDone = False
 
 	def reset(self):
 		self.visited = False
@@ -29,14 +44,20 @@ class MazeNode:
 
 class Maze:
 	def __init__(self, cols, rows):
-		self.maze = [[MazeNode(i,j) for j in range(cols)] for i in range(rows)]
+		
 		# maze[i][j] is the node at row i and column j
 		self.cols = cols
 		self.rows = rows
 
+		self.visitedNodes = 0
+		self.maze = [[MazeNode(i,j, self) for j in range(cols)] for i in range(rows)]
+
 		self.currNode = self.maze[0][0]
 		self.currNode.isCurr = True
-		self.currNode.visit()
+
+		# size of the grid in the graphics
+		self.gridSize = 50
+		self.win=GraphWin("Maze", self.gridSize*self.cols, self.gridSize*self.rows)
 
 		#handle edges so robot doesnt go off the map
 		for i in range(rows):
@@ -45,6 +66,8 @@ class Maze:
 		for i in range(cols):
 			self.maze[0][i].up = False
 			self.maze[rows-1][i].down = False
+
+		self.notDone = True
 
 	def addWall(self, x1, y1, x2, y2):
 		# creates wall between the nodes specified by these coordinates
@@ -88,6 +111,79 @@ class Maze:
 			#both coordinates are the same
 			print("coordinates must be different")
 		return
+
+	def addRandomWalls(self, num):
+		for i in range(num):
+			row = random.randint(0,self.rows-1)
+			col = random.randint(0,self.col-1)
+			direction = random.randint(1,4)
+			if(direction==1):
+				#up
+				pass
+			elif(direction==2):
+				#down
+				pass
+			elif(direction==3):
+				#right
+				pass
+			elif(direction==4):
+				#left
+				pass
+
+
+	def drawMaze(self):
+		win = self.win
+		for i in range(self.rows):
+			for j in range(self.cols):
+				rect = Rectangle(Point(j*self.gridSize, i*self.gridSize), 
+					Point(j*self.gridSize + self.gridSize, i*self.gridSize +self.gridSize))
+
+				mazeNode = self.maze[i][j]
+
+				if(mazeNode.isCurr):
+					rect.setFill("blue")
+				elif(mazeNode.visited):
+					rect.setFill("red")
+				else:
+					rect.setFill("white")
+				rect.draw(win)
+				
+				#draw walls
+				if(not mazeNode.left):
+					#left wall
+					wall = Line(Point(j*self.gridSize, i*self.gridSize),
+						Point(j*self.gridSize, i*self.gridSize + self.gridSize))
+					wall.setWidth(5)
+					wall.setFill("brown")
+					wall.draw(win)
+				if(not mazeNode.right):
+					#right wall
+					wall = Line(Point(j*self.gridSize+ self.gridSize, i*self.gridSize),
+						Point(j*self.gridSize+ self.gridSize, i*self.gridSize + self.gridSize))
+					wall.setWidth(5)
+					wall.setFill("brown")
+					wall.draw(win)
+				if(not mazeNode.down):
+					#down wall
+					wall = Line(Point(j*self.gridSize, i*self.gridSize+ self.gridSize),
+						Point(j*self.gridSize+self.gridSize, i*self.gridSize+ self.gridSize))
+					wall.setWidth(5)
+					wall.setFill("brown")
+					wall.draw(win)
+				if(not mazeNode.up):
+					#up wall
+					wall = Line(Point(j*self.gridSize, i*self.gridSize),
+						Point(j*self.gridSize+self.gridSize, i*self.gridSize))
+					wall.setWidth(5)
+					wall.setFill("brown")
+					wall.draw(win)
+				
+
+		#win.getMouse()
+		sleep(0.05)
+		#win.close()
+		win.update()
+			
 
 	def printMaze(self):
 		for i in range(self.rows):
@@ -149,40 +245,38 @@ class Maze:
 		stack.append(self.currNode)
 		while(len(stack)!=0):
 			node = stack.pop()
-			node.visit()
-			print("at", node.x, node.y)
-			print("left", node.left)
-			print("right", node.right)
-			print("up", node.up)
-			print("down", node.down)
-			if(node.left):
-				left = self.maze[node.x][node.y-1]
-				if(not left.visited):
-					print("adding left", node.x, node.y-1)
-					stack.append(left)
-			if(node.right):
-				right = self.maze[node.x][node.y+1]
-				if(not right.visited):
-					print("adding right", node.x, node.y+1)
-					stack.append(right)
-			if(node.up):
-				up = self.maze[node.x-1][node.y]
-				if(not up.visited):
-					print("adding up", node.x-1, node.y)
-					stack.append(up)
-			if(node.down):
-				down = self.maze[node.x+1][node.y]
-				if(not down.visited):
-					print("adding down", node.x+1, node.y)
-					stack.append(down)
-			self.printMaze()
+			if(not node.visited):
+				node.visit()
+				self.printMaze()
+				self.drawMaze()
+				if(node.down):
+					down = self.maze[node.x+1][node.y]
+					if(not down.visited):
+						print("adding down", node.x+1, node.y)
+						stack.append(down)
+				if(node.up):
+					up = self.maze[node.x-1][node.y]
+					if(not up.visited):
+						print("adding up", node.x-1, node.y)
+						stack.append(up)
+				if(node.right):
+					right = self.maze[node.x][node.y+1]
+					if(not right.visited):
+						print("adding right", node.x, node.y+1)
+						stack.append(right)
+				if(node.left):
+					left = self.maze[node.x][node.y-1]
+					if(not left.visited):
+						print("adding left", node.x, node.y-1)
+						stack.append(left)
+			
 
-maze = Maze(4,4)
-maze.addWall(0,0,1,0)
-maze.addWall(1,2,1,3)
-#maze.printMaze()
-maze.DFS()
-
+if __name__ == '__main__':
+	maze = Maze(4,5)
+	maze.addWall(0,0,1,0)
+	maze.addWall(1,2,1,3)
+	#maze.printMaze()
+	maze.DFS()
 
 
 
