8b6a5b766afb2eb5ccd792fa2dd6a5a1921490b3
Firehiwot <31547603+Firehiwot@users.noreply.github.com>
2017-11-19 16:09:47 -0500

    Create RECEIVE.v

---
+++ docs/milestone4/RECEIVE.v
@@ -0,0 +1,32 @@
+module RECEIVE (
+	CLOCK,
+	DATA,
+	ENABLE,
+	OUT
+);
+
+input CLOCK;
+input DATA;
+input ENABLE;
+
+output [15:0] OUT;
+
+reg [3:0] index;
+reg [15:0] accumulate;
+reg [15:0] pastOut;
+
+assign OUT = pastOut;
+
+always @(posedge CLOCK) begin
+	if(ENABLE) begin
+		accumulate[index] <= DATA;
+		if(index == 4'b1111) begin
+			pastOut <= accumulate;
+		end
+		else begin
+			pastOut <= 16'b1111111111111111;
+		end
+		index <= index + 1;
+	end
+end
+endmodule
